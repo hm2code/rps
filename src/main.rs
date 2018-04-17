00dfcb717770bc04143f67d8bd0a590a9e42bc67
hm2code <hm2code@gmail.com>
2018-04-16 21:31:42 -0700

    A quick test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+struct Rand {
+    state: u32,
+}
+
+impl Rand {
+    fn with_seed(seed: u32) -> Rand {
+        Rand { state: seed }
+    }
+
+    fn next_u32(&mut self) -> u32 {
+        // xorshift32 - https://en.wikipedia.org/wiki/Xorshift
+        let mut x = self.state;
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+        self.state = x;
+        x
+    }
+
+    fn rps(&mut self) -> RPS {
+        let r = self.next_u32() & 0xFF;
+        if r < 85 {
+            RPS::Rock
+        } else if r < 170 {
+            RPS::Paper
+        } else {
+            RPS::Scissors
+        }
+    }
+}
+
+#[derive(Debug)]
+enum RPS {
+    Rock,
+    Paper,
+    Scissors,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rand = Rand::with_seed(1234);
+    for _ in 0..20 {
+        println!("rps: {:?}", rand.rps());
+    }
 }
